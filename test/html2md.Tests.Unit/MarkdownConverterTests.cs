2e4dc61196e57d3233a498ed821aa3d86beec157
Mike Goatly <mikegoatly@hotmail.com>
2020-08-05 08:46:07 +0100

    Added tests

--- test/html2md.Tests.Unit/MarkdownConverterTests.cs
+++ test/html2md.Tests.Unit/MarkdownConverterTests.cs
@@ -38,6 +38,110 @@ namespace Html2md.Tests.Unit
         }
 
         [Fact]
+        public async Task ShouldConvertSimpleUnorderedList()
+        {
+            await TestConverter(
+                "<ul><li>One</li><li>Two</li></ul>",
+                @"
+- One
+- Two
+
+");
+        }
+
+        [Fact]
+        public async Task ShouldConvertSimpleOrderedList()
+        {
+            await TestConverter(
+                "<ol><li>One</li><li>Two</li></ol>",
+                @"
+1. One
+1. Two
+
+");
+        }
+
+        [Fact]
+        public async Task ShouldConvertHeadersAtCorrectLevel()
+        {
+            for (var i = 1; i <= 6; i++)
+            {
+                await TestConverter(
+                    $"<h{i}>Header</h{i}>",
+                    $@"
+{new string('#', i)} Header
+
+");
+            }
+        }
+
+        [Fact]
+        public async Task ShouldConvertStylingInHeaders()
+        {
+            await TestConverter(
+                    "<h3>Header <em>Two</em></h3>",
+                    @"
+### Header *Two*
+
+");
+        }
+
+        [Fact]
+        public async Task ShouldConverTableWithHeaderRow()
+        {
+            await TestConverter(
+                    @"<table>
+    <thead>
+        <tr>
+            <th>Col 1</th>
+            <th>Col 2</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1-1</td>
+            <td>1-2</td>
+        </tr>
+        <tr>
+            <td>2-1</td>
+            <td>2-2</td>
+        </tr>
+    </tbody>
+</table>",
+                    $@"
+|Col 1|Col 2|
+|-|-|
+|1-1|1-2|
+|2-1|2-2|
+
+");
+        }
+
+        [Fact]
+        public async Task ShouldConverTableWithNoHeaderRowUsingFirstRowAsHeader()
+        {
+            await TestConverter(
+                    @"<table>
+    <tbody>
+        <tr>
+            <td>1-1</td>
+            <td>1-2</td>
+        </tr>
+        <tr>
+            <td>2-1</td>
+            <td>2-2</td>
+        </tr>
+    </tbody>
+</table>",
+                    $@"
+|1-1|1-2|
+|-|-|
+|2-1|2-2|
+
+");
+        }
+
+        [Fact]
         public async Task ShouldConvertPreWithoutClass()
         {
             await TestConverter(
